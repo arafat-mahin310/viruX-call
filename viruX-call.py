7714893efb19f0ad131273a23376c140c4324ac6
Arafat-Mahin310 <80392241+arafat-mahin310@users.noreply.github.com>
2023-03-10 12:35:36 +0600

    Add files via upload

---
+++ viruX-call.py
@@ -0,0 +1,98 @@
+import os
+import sys
+import time 
+from time import sleep
+
+
+#### colours ####
+# B='\033[1;94m'
+# R='\033[1;91m'
+# G='\033[1;92m'
+# W='\033[1;97m'
+# S='\033[1;96m'
+# P='\033[1;95m'
+# Y='\033[1;93m'
+B='\033[1;94m'
+R='\033[1;91m'
+G='\033[1;92m'
+W='\033[1;97m'
+S='\033[1;96m'
+P='\033[1;95m'
+Y='\033[1;93m'
+
+
+
+os.system('clear')
+
+print( """
+
+Oo      oO           o      'O                  o      O 
+O O    o o           O       o o                 O    o  
+o  o  O  O           o       O                    o  O   
+O   Oo   O           o       o                     oO    
+O        o `OoOo.    O      O' O  `OoOo. O   o     Oo    
+o        O  o        `o    o   o   o     o   O    o  o   
+o        O  O     oO  `o  O    O   O     O   o   O    O  
+O        o  o     Oo   `o'     o'  o     `OoO'o O      o 
+                                                         
+                                                         
+
+\033[1;92m★================================================
+★[✯] DEVELOPER: Arafat Bin Mahin                                         ★[✯] FB GROUP : TERMUX HACKER SHAMIM
+★[✯] FACEBOOK : Arafat Mahin 
+★[✯] WHATSAPP : +8801925798757
+★❌❌This tools is very dangerous❌❌
+★ Pls Don't  use it illegally😎
+★ I wont be responsible for any illegal use👈👈
+★THIS TOOLS IS A BANGLADESH CALL BOMBING TOOLS                                                                                                         
+\033[1;94m""") 
+
+print("""\033[1;92m[✓]\033[1;97mplease Follow My Facebook \033[1;92m[✓] """)
+
+print("""\033[1;96myou will need to login to this tools to continue""")
+time.sleep(0.5)
+CorrectUsername = "VVIRUX"
+CorrectPassword = "MR.MAHIN"
+username = input("\033[1;95mEnter the  username: ")
+time.sleep(0.2)
+password = input("\033[1;93mEnter the  password: ")
+time.sleep(0.2)
+if (username == CorrectUsername): pass 
+elif (username != CorrectUsername): 
+    exit ("\033[1;92mAcess denied \033[1;91mX")
+if (password == CorrectPassword): pass
+elif (password != CorrectPassword):
+    exit ("\033[1;92mAcess denied \033[1;91mX")
+print("""\033[1;92m[✓]\033[1;97mAccess granted""")
+time.sleep(2)
+print("""\033[1;91m[*]\033[1;95mwelcome to my tool""")
+time.sleep(2)
+print("""\033[1;93m[*]\033[1;96m😁😁😁😁😁😁😁😁""")
+time.sleep(2)
+
+
+os.system('xdg-open https://www.facebook.com/arafatmahin310')
+print("""[*]YOUR CALL BOMBING LINK:- http://pikachu-call-bomber.ezyro.com/babyyy.php?phone=
+                                        
+\033[1;94m""")                                         
+                                     
+
+MSG()
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
